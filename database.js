a8c75dad632735d2fc6797fa71b01ce01851f234
Konrad Szychowiak <konrad.szychowiak@gmail.com>
2022-02-07 19:01:36 +0100

    Migrate from mysql to sqlite3

--- database.js
+++ database.js
@@ -1,67 +1,67 @@
 const express = require('express');
-const mysql = require('mysql');
 const bodyParser = require('body-parser');
-
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: ""
+const db = require('better-sqlite3')('database.sqlite3', {
+    verbose: console.log
 });
-
-db.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
-
 const app = express()
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(function (req, res) {
-    res.setHeader('Content-Type', 'text/plain')
-    res.write('you posted:\n')
-    res.end(JSON.stringify(req.body, null, 2))
-})
+// app.use(function (req, res) {
+//     res.setHeader('Content-Type', 'text/plain')
+//     res.write('you posted:\n')
+//     res.end(JSON.stringify(req.body, null, 2))
+// })
 
-app.get('/createdb', (req,res) => {
-    let sql = 'CREATE DATABASE nodemysql'
-    db.query(sql,err => {
-        if(err) {
-            throw err
-        }
-        res.send("Database created");
-    })
+app.get('/createdb', (req, res) => {
+    // db.exec(`-- drop table if exists log;`)
+    // db.exec(`drop table if exists products;`)
+    db.exec(`create table products
+             (
+                 id number primary key,
+                 name
+             );`)
+    // db.exec(``)
+    res.send({ message: "created" })
 })
 
 app.listen('3000', () => {
     console.log('Server Started on port 3000')
 })
 
-const dodaj = async(ctx) => {
-    const result = await sql`update database set ilosc = ilosc + 1;`
-}
+app.post('/update', (req, res) => {
+    console.log(req.body)
+    // const answer = db.prepare(``).all()
+    res.send({ message: 'added product' })
+})
 
-const usun = async(ctx) => {
-    const result = await sql`update database set ilosc = ilosc - 1;`
-}
+app.get('/list', (req, res) => {
+    const answer = db.prepare(`select * from products;`).all()
+    console.log(answer)
+    res.send({ message: 'list' })
+})
 
-const lista = async(ctx) => {
-    const result = await sql`select * from database order by kategoria, nazwa;`
-    if (result) ctx.body = result
-}
-
-const jeden = async(ctx) => {
-    const {code_id} = ctx.params
-    const result = await sql`select * from database where code_id = ${code_id};`
-    if (result) ctx.body = result
-}
-
-export default new Router()
-    .post('/', dodaj)
-    .post('/', usun)
-    .get('', lista)
-    .get('/:code_id', jeden)
+// const usun = async(ctx) => {
+//     const result = await sql`update database set ilosc = ilosc - 1;`
+// }
+//
+// const lista = async(ctx) => {
+//     const result = await sql`select * from database order by kategoria, nazwa;`
+//     if (result) ctx.body = result
+// }
+//
+// const jeden = async(ctx) => {
+//     const {code_id} = ctx.params
+//     const result = await sql`select * from database where code_id = ${code_id};`
+//     if (result) ctx.body = result
+// }
+//
+// export default new Router()
+//     .post('/', dodaj)
+//     .post('/', usun)
+//     .get('', lista)
+//     .get('/:code_id', jeden)
