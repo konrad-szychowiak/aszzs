dd14ced54f95613337b109fb8400bb5a60e9c727
JAuguscik <92777170+JAuguscik@users.noreply.github.com>
2022-02-07 19:57:10 +0100

    Update application.js

--- application.js
+++ application.js
@@ -19,7 +19,7 @@ Quagga.init({
         target: document.querySelector('#container')
     },
     decoder : {
-        readers : ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader", "2of5_reader", "code_93_reader"]
+        readers : ["ean_reader", "ean_8_reader"]
     }
 }, function(err) {
     if (err) {
